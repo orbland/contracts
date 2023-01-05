ef67cc40ff388cca94eb2083a1f6600f9f0ed596
Jonas Lekevicius <jonas@lekevicius.com>
2023-01-05 15:02:43 +0200

    Deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,29 @@
-import { HardhatUserConfig } from 'hardhat/config'
-import '@nomicfoundation/hardhat-toolbox'
-import '@nomiclabs/hardhat-solhint'
-import 'hardhat-contract-sizer'
-import 'hardhat-gas-reporter'
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "@nomiclabs/hardhat-solhint"
+import "hardhat-contract-sizer"
+import "hardhat-gas-reporter"
+
+import * as dotenv from "dotenv"
+dotenv.config()
 
 const coinmarketcapKey: string | undefined = process.env.CMC_API_KEY
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.17',
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY as string],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
   gasReporter: {
     enabled: true,
-    currency: 'USD',
+    currency: "USD",
     coinmarketcap: coinmarketcapKey,
   },
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,24 @@
-import { ethers } from 'hardhat'
+import { ethers, network, run } from "hardhat"
 
 async function main() {
-  const EricOrb = await ethers.getContractFactory('EricOrb')
+  const EricOrb = await ethers.getContractFactory("EricOrb")
   const ericOrb = await EricOrb.deploy()
 
   await ericOrb.deployed()
+
+  if (network.name === "goerli") {
+    const WAIT_BLOCK_CONFIRMATIONS = 6
+    await ericOrb.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS)
+
+    console.log(`Contract deployed to ${ericOrb.address} on ${network.name}`)
+
+    console.log(`Verifying contract on Etherscan...`)
+
+    await run(`verify:verify`, {
+      address: ericOrb.address,
+      constructorArguments: [],
+    })
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere and properly handle errors.
