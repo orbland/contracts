f290aca64c9b2be991763d6222c3323080c7d738
Jonas Lekevicius <jonas@lekevicius.com>
2023-01-05 15:56:22 +0200

    Optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,15 @@ dotenv.config()
 const coinmarketcapKey: string | undefined = process.env.CMC_API_KEY
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
   networks: {
     hardhat: {},
     goerli: {
