3756ff82ee7e0a6da2f05ea8e84a914d8e62dfc5
Jonas Lekevicius <jonas@lekevicius.com>
2023-01-07 22:37:04 +0200

    Dev version of the contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,32 @@
 import { ethers, network, run } from "hardhat"
 
 async function main() {
-  const EricOrb = await ethers.getContractFactory("EricOrb")
-  const ericOrb = await EricOrb.deploy()
+  console.log(network.name)
+  if (network.name === "localhost") {
+    console.log("localhost!")
+    const EricOrb = await ethers.getContractFactory("EricOrbDev")
+    const ericOrb = await EricOrb.deploy()
 
-  await ericOrb.deployed()
+    await ericOrb.deployed()
+  } else {
+    const EricOrb = await ethers.getContractFactory("EricOrb")
+    const ericOrb = await EricOrb.deploy()
 
-  if (network.name === "goerli") {
-    const WAIT_BLOCK_CONFIRMATIONS = 6
-    await ericOrb.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS)
+    await ericOrb.deployed()
 
-    console.log(`Contract deployed to ${ericOrb.address} on ${network.name}`)
+    if (network.name === "goerli") {
+      const WAIT_BLOCK_CONFIRMATIONS = 6
+      await ericOrb.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS)
 
-    console.log(`Verifying contract on Etherscan...`)
+      console.log(`Contract deployed to ${ericOrb.address} on ${network.name}`)
 
-    await run(`verify:verify`, {
-      address: ericOrb.address,
-      constructorArguments: [],
-    })
+      console.log(`Verifying contract on Etherscan...`)
+
+      await run(`verify:verify`, {
+        address: ericOrb.address,
+        constructorArguments: [],
+      })
+    }
   }
 }
 
