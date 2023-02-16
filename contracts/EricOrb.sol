4086661ec5cface4c5ac55aefc39f4edcb0a06a2
Odysseas.eth <odyslam@gmail.com>
2023-02-16 15:57:54 -0300

    ref: remove zero price check from purchase()

--- contracts/EricOrb.sol
+++ contracts/EricOrb.sol
@@ -704,10 +704,6 @@ contract EricOrb is ERC721, Ownable {
       revert CurrentPriceIncorrect(currentPrice, _price);
     }
 
-    if (newPrice == 0) {
-      revert InvalidNewPrice(newPrice);
-    }
-
     address holder = ERC721.ownerOf(ERIC_ORB_ID);
 
     if (_msgSender() == holder) {
