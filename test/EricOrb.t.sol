5a50c678035533031c69d8826d9eeda3ff6d6072
Odysseas.eth <odyslam@gmail.com>
2023-02-19 22:47:56 -0300

    test: startAuction, bid

--- test/EricOrb.t.sol
+++ test/EricOrb.t.sol
@@ -5,8 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {EricOrbHarness} from "./harness/EricOrbHarness.sol";
 import {EricOrb} from "contracts/EricOrb.sol";
 
-contract EricOrbTest is Test{
-
+contract EricOrbTest is Test {
     EricOrbHarness orb;
 
     address user;
@@ -14,6 +13,7 @@ contract EricOrbTest is Test{
     function setUp() public {
         orb = new EricOrbHarness();
         user = address(0xBEEF);
+        vm.deal(user, 100 ether);
     }
 
     function prankAndBid(address bidder, uint256 bidAmount) internal {
@@ -63,7 +63,7 @@ contract EricOrbTest is Test{
 
         assertEq(orb.workaround_orbId(), 69);
         assertEq(orb.workaround_infinity(), type(uint256).max);
-        assertEq(orb.workaround_maxPrice(), 2 ** 128 );
+        assertEq(orb.workaround_maxPrice(), 2 ** 128);
         assertEq(orb.workaround_baseUrl(), "https://static.orb.land/eric/");
     }
 
@@ -90,7 +90,6 @@ contract EricOrbTest is Test{
         amount = bound(amount, 0, type(uint224).max);
         assertEq(orb.fundsRequiredToBid(amount), amount + (amount * 1_000 / 10_000));
     }
-
 
     function test_startAuctionOnlyOrbIssuer() public {
         vm.prank(address(0xBEEF));
@@ -135,12 +134,128 @@ contract EricOrbTest is Test{
 
     function test_bidOnlyDuringAuction() public {
         uint256 bidAmount = 0.6 ether;
+        uint256 finalAmount = orb.fundsRequiredToBid(bidAmount);
+        vm.deal(user, finalAmount);
         vm.expectRevert(EricOrb.AuctionNotRunning.selector);
-        orb.bid{value: 1 ether}(bidAmount);
+        vm.prank(user);
+        orb.bid{value: finalAmount}(bidAmount);
         orb.startAuction();
         assertEq(orb.winningBid(), 0 ether);
-        orb.bid{value: 1 ether}(bidAmount);
+        prankAndBid(user, bidAmount);
         assertEq(orb.winningBid(), bidAmount);
     }
 
+    function test_bidUsesTotalFunds() public {
+        orb.deposit{value: 1 ether}();
+        orb.startAuction();
+        vm.prank(user);
+        assertEq(orb.winningBid(), 0 ether);
+        prankAndBid(user, 0.5 ether);
+        assertEq(orb.winningBid(), 0.5 ether);
+    }
+
+    function test_bidRevertsIfLtMinimumBid() public {
+        orb.startAuction();
+        // minimum bid will be the STARTING_PRICE
+        uint256 amount = orb.minimumBid() - 1;
+        vm.expectRevert(abi.encodeWithSelector(EricOrb.InsufficientBid.selector, amount, orb.minimumBid()));
+        vm.prank(user);
+        orb.bid{value: amount}(amount);
+
+        // Add back + 1 to amount
+        amount++;
+        // will not revert
+        vm.prank(user);
+        orb.bid{value: orb.fundsRequiredToBid(amount)}(amount);
+        assertEq(orb.winningBid(), amount);
+
+        // minimum bid will be the winning bid + MINIMUM_BID_STEP
+        amount = orb.minimumBid() - 1;
+        vm.expectRevert(abi.encodeWithSelector(EricOrb.InsufficientBid.selector, amount, orb.minimumBid()));
+        vm.prank(user);
+        orb.bid{value: amount}(amount);
+    }
+
+    function test_bidRevertsIfLtFundsRequired() public {
+        orb.startAuction();
+        uint256 amount = orb.minimumBid();
+        uint256 funds = orb.fundsRequiredToBid(amount) - 1;
+        vm.expectRevert(abi.encodeWithSelector(EricOrb.InsufficientFunds.selector, funds, funds + 1));
+        vm.prank(user);
+        orb.bid{value: funds}(amount);
+
+        funds++;
+        vm.prank(user);
+        // will not revert
+        orb.bid{value: funds}(amount);
+        assertEq(orb.winningBid(), amount);
+    }
+
+    event NewBid(address indexed from, uint256 price);
+
+    function test_bidSetsCorrectState() public {
+        orb.startAuction();
+        uint256 amount = orb.minimumBid();
+        uint256 funds = orb.fundsRequiredToBid(amount);
+        assertEq(orb.winningBid(), 0 ether);
+        assertEq(orb.winningBidder(), address(0));
+        assertEq(orb.fundsOf(user), 0);
+        assertEq(address(orb).balance, 0);
+        uint256 endTime = orb.endTime();
+
+        vm.expectEmit(true, false, false, true);
+        emit NewBid(user, amount);
+        prankAndBid(user, amount);
+
+        assertEq(orb.winningBid(), amount);
+        assertEq(orb.winningBidder(), user);
+        assertEq(orb.fundsOf(user), funds);
+        assertEq(address(orb).balance, funds);
+        assertEq(orb.endTime(), endTime);
+    }
+
+    mapping(address => uint256) fundsOfUser;
+
+    function testFuzz_bidSetsCorrectState(address[16] memory users, uint128[16] memory amounts) public {
+        orb.startAuction();
+        uint256 contractBalance;
+        for (uint256 i = 1; i < 16; i++) {
+            uint256 amount = bound(amounts[i], orb.minimumBid(), orb.minimumBid() + 1_000_000_000);
+            user = users[i];
+            vm.assume(user != address(0));
+
+            uint256 funds = orb.fundsRequiredToBid(amount);
+
+            fundsOfUser[user] += funds;
+
+            vm.expectEmit(true, false, false, true);
+            emit NewBid(user, amount);
+            prankAndBid(user, amount);
+            contractBalance += funds;
+
+            assertEq(orb.winningBid(), amount);
+            assertEq(orb.winningBidder(), user);
+            assertEq(orb.fundsOf(user), fundsOfUser[user]);
+            assertEq(address(orb).balance, contractBalance);
+        }
+    }
+
+    function test_bidExtendsAuction() public {
+        assertEq(orb.endTime(), 0);
+        orb.startAuction();
+        uint256 amount = orb.minimumBid();
+        // endTime = block.timestamp + MINIMUM_AUCTION_DURATION
+        uint256 endTime = orb.endTime();
+        // set block.timestamp to endTime - BID_AUCTION_EXTENSION
+        vm.warp(endTime - orb.BID_AUCTION_EXTENSION());
+        prankAndBid(user, amount);
+        // didn't change because block.timestamp + BID_AUCTION_EXTENSION  = endTime
+        assertEq(orb.endTime(), endTime);
+
+        vm.warp(endTime - orb.BID_AUCTION_EXTENSION() + 50);
+        amount = orb.minimumBid();
+        prankAndBid(user, amount);
+        // change because block.timestamp + BID_AUCTION_EXTENSION + 50 >  endTime
+        assertEq(orb.endTime(), endTime + 50);
+    }
 }
