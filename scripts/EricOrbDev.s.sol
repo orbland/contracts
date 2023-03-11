a45dc1c3b0dd777ea0f61a7d2c4c861024a99622
Jonas Lekevicius <jonas@lekevicius.com>
2023-03-11 13:20:45 +0200

    Goodbye dev contract

--- scripts/EricOrbDev.s.sol
+++ scripts/EricOrbDev.s.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "forge-std/Script.sol";
-import "src/EricOrbDev.sol";
+import {Script} from "forge-std/Script.sol";
+import {EricOrb} from "src/EricOrb.sol";
 
 contract DeployEricOrbDev is Script {
     function run() external {
         vm.startBroadcast();
-        new EricOrbDev();
+        new EricOrb(
+            5 minutes, // cooldown
+            5 minutes, // responseFlaggingPeriod
+            5 minutes, // minimumAuctionDuration
+            30 seconds // bidAuctionExtension
+        );
         vm.stopBroadcast();
     }
 }
