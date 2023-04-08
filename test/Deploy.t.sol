2f2510b646fa8b8a0502505b62095b8996f525bc
Jonas Lekevicius <jonas@lekevicius.com>
2023-04-08 23:11:22 +0300

    Fixes for Goerli deploy (#78)
    Rename scripts to to script, set goerli params

--- test/Deploy.t.sol
+++ test/Deploy.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployLocal} from "../scripts/DeployLocal.s.sol";
+import {DeployLocal} from "../script/DeployLocal.s.sol";
 import {EricOrb} from "src/EricOrb.sol";
 
 contract DeployMainnetTest is Test {
