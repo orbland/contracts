164d01a6bb652c7b64e2a51cb37a2c3fbc599526
Jonas Lekevicius <jonas@lekevicius.com>
2023-05-29 10:50:04 +0300

    Deploy scripts updated to Orb.sol
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

--- script/DeploySepolia.s.sol
+++ script/DeploySepolia.s.sol
@@ -4,18 +4,53 @@ pragma solidity ^0.8.17;
 import {DeployBase} from "./DeployBase.s.sol";
 
 contract DeploySepolia is DeployBase {
-    address[] private beneficiaryAddresses = [
+    address[] public beneficiaryAddresses = [
         0xE48C655276C23F1534AE2a87A2bf8A8A6585Df70, // ercwl.eth
         0x9F49230672c52A2b958F253134BB17Ac84d30833, // jonas.eth
         0xf374CE39E4dB1697c8D0D77F91A9234b2Fd55F62 // odysseas
     ];
-    uint256[] private beneficiaryShares = [65, 20, 15];
+    uint256[] public beneficiaryShares = [65, 20, 15];
 
-    // would be ercwl.eth for mainnet
-    address private immutable creatorAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    uint256 private immutable tokenId = 1;
+    address public immutable creatorAddress = 0xC0A00c8c9EF6fe6F0a79B8a616183384dbaf8EC8;
 
-    constructor() DeployBase(beneficiaryAddresses, beneficiaryShares, creatorAddress, tokenId) 
+    string public orbName = "Test Orb";
+    string public orbSymbol = "ORB";
+    uint256 public immutable tokenId = 1;
+
+    string public oath = "My orb is my promise, and my promise is my orb.";
+    uint256 public immutable honoredUntil = 1_700_000_000;
+
+    uint256 public immutable auctionStartingPrice = 0.1 ether;
+    uint256 public immutable auctionMinimumBidStep = 0.1 ether;
+    uint256 public immutable auctionMinimumDuration = 15 minutes;
+    uint256 public immutable auctionBidExtension = 1 minutes;
+
+    uint256 public immutable holderTaxNumerator = 10_00;
+    uint256 public immutable royaltyNumerator = 10_00;
+
+    uint256 public immutable cooldown = 5 minutes;
+
+    uint256 public immutable cleartextMaximumLength = 280;
+
+    constructor()
+        DeployBase(
+            beneficiaryAddresses,
+            beneficiaryShares,
+            creatorAddress,
+            orbName,
+            orbSymbol,
+            tokenId,
+            oath,
+            honoredUntil,
+            auctionStartingPrice,
+            auctionMinimumBidStep,
+            auctionMinimumDuration,
+            auctionBidExtension,
+            holderTaxNumerator,
+            royaltyNumerator,
+            cooldown,
+            cleartextMaximumLength
+        )
     /* solhint-disable no-empty-blocks */
     {}
     /* solhint-enable no-empty-blocks */
