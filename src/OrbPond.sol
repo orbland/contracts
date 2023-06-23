12e7f25c63e7618e826bf6af7dd6ccc811d226b1
Jonas Lekevicius <jonas@lekevicius.com>
2023-06-23 16:31:23 +0300

    auctionKeeperMinimumDuration, for different length keeper auctions
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

--- src/OrbPond.sol
+++ src/OrbPond.sol
@@ -45,20 +45,22 @@ contract OrbPond is Ownable {
     }
 
     /// @notice  Configures most Orb's parameters in one transaction. Used to initially set up the Orb.
-    /// @param   orbId                   Id of the Orb to configure.
-    /// @param   auctionStartingPrice    Starting price of the Orb's auction.
-    /// @param   auctionMinimumBidStep   Minimum difference between bids in the Orb's auction.
-    /// @param   auctionMinimumDuration  Minimum duration of the Orb's auction.
-    /// @param   auctionBidExtension     Auction duration extension for late bids during the Orb auction.
-    /// @param   keeperTaxNumerator      Harberger tax numerator of the Orb, in basis points.
-    /// @param   royaltyNumerator        Royalty numerator of the Orb, in basis points.
-    /// @param   cooldown                Cooldown of the Orb in seconds.
-    /// @param   cleartextMaximumLength  Invocation cleartext maximum length for the Orb.
+    /// @param   orbId                         Id of the Orb to configure.
+    /// @param   auctionStartingPrice          Starting price of the Orb's auction.
+    /// @param   auctionMinimumBidStep         Minimum difference between bids in the Orb's auction.
+    /// @param   auctionMinimumDuration        Minimum duration of the Orb's auction.
+    /// @param   auctionKeeperMinimumDuration  Minimum duration of the Orb's auction.
+    /// @param   auctionBidExtension           Auction duration extension for late bids during the Orb auction.
+    /// @param   keeperTaxNumerator            Harberger tax numerator of the Orb, in basis points.
+    /// @param   royaltyNumerator              Royalty numerator of the Orb, in basis points.
+    /// @param   cooldown                      Cooldown of the Orb in seconds.
+    /// @param   cleartextMaximumLength        Invocation cleartext maximum length for the Orb.
     function configureOrb(
         uint256 orbId,
         uint256 auctionStartingPrice,
         uint256 auctionMinimumBidStep,
         uint256 auctionMinimumDuration,
+        uint256 auctionKeeperMinimumDuration,
         uint256 auctionBidExtension,
         uint256 keeperTaxNumerator,
         uint256 royaltyNumerator,
@@ -67,7 +69,11 @@ contract OrbPond is Ownable {
         uint256 cleartextMaximumLength
     ) external onlyOwner {
         orbs[orbId].setAuctionParameters(
-            auctionStartingPrice, auctionMinimumBidStep, auctionMinimumDuration, auctionBidExtension
+            auctionStartingPrice,
+            auctionMinimumBidStep,
+            auctionMinimumDuration,
+            auctionKeeperMinimumDuration,
+            auctionBidExtension
         );
         orbs[orbId].setFees(keeperTaxNumerator, royaltyNumerator);
         orbs[orbId].setCooldown(cooldown, flaggingPeriod);
