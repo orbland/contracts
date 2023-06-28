03acc616cbf77a5f606df53b39aeff26825e457a
Jonas Lekevicius <jonas@lekevicius.com>
2023-06-28 15:35:52 +0300

    Updated Oath fields and logging Oath hash
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

--- script/OrbScenario.s.sol
+++ script/OrbScenario.s.sol
@@ -14,7 +14,7 @@ contract OrbScenario is Script {
 
     /* solhint-disable quotes,max-line-length */
     string public oath =
-        '{"oath":"My orb is my promise, and my promise is my orb.","allowPrivateInvocations":true,"allowPrivateResponses":true,"allowRevealOwnResponses":true,"allowReadPastResponses":true,"allowRevealPastResponses":true}';
+        '{"oath":"My orb is my promise, and my promise is my orb.","allowPublicInvocations":true,"allowPrivateInvocations":true,"allowPrivateResponses":true,"allowReadPastInvocations":true,"allowRevealOwnInvocations":true,"allowRevealPastInvocations":true}';
     /* solhint-enable quotes,max-line-length */
     uint256 public immutable honoredUntil = 1_700_000_000;
     uint256 public immutable responsePeriod = 7 * 24 * 60 * 60;
@@ -34,10 +34,11 @@ contract OrbScenario is Script {
         orb = Orb(orbPond.orbs(0));
         console.log("orb address", address(orb));
 
+        console.log("oath hash");
+        console.logBytes32(keccak256(abi.encodePacked(oath)));
+
         vm.startBroadcast(creatorKey);
-
         orb.swearOath(keccak256(abi.encodePacked(oath)), honoredUntil, responsePeriod);
-
         vm.stopBroadcast();
     }
 }
