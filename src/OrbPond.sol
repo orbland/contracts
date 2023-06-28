2affa41b7835836fc9355c72c62783abe0a58aaf
Jonas Lekevicius <jonas@lekevicius.com>
2023-06-28 22:00:42 +0300

    Upgrading solidity version
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

--- src/OrbPond.sol
+++ src/OrbPond.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import {Orb} from "./Orb.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
