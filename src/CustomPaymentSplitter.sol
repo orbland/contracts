771f5939dfb0545391995a5aae65b8d31afb5d3e
Jonas Lekevicius <jonas@lekevicius.com>
2023-07-05 01:38:14 +0300

    Updated CustomPaymentSplitter documentation
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

--- src/CustomPaymentSplitter.sol
+++ src/CustomPaymentSplitter.sol
@@ -6,22 +6,18 @@ import {PaymentSplitterUpgradeable} from
 
 /// @title   CustomPaymentSplitter - Payment Splitter with initializer
 /// @author  Jonas Lekevicius
-/// @dev     An upgradeability mechanism designed for UUPS proxies. The functions included here can perform an upgrade
-///          of an `ERC1967Proxy`, when this contract is set as the implementation behind such a proxy. This is a
-///          modified version of the OpenZeppelin Contract `UUPSUpgradeable` contract that does not expose any public
-///          functions, to allow custom upgradeability logic to be implemented in the `Orb` contract.
-///          Also, replaces string errors with custom errors.
+/// @dev     This is a non-abstract version of the OpenZeppelin Contract `PaymentSplitterUpgradeable` contract that does
+///          implements an initializer, and has a constructor to disable the initializer on base deployment. Meant to be
+///          used as an implementation to a EIP-1167 clone factory.
 contract PaymentSplitter is PaymentSplitterUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    /// @dev    When deployed, contract mints the only token that will ever exist, to itself.
-    ///         This token represents the Orb and is called the Orb elsewhere in the contract.
-    ///         `Ownable` sets the deployer to be the `owner()`, and also the creator in the Orb context.
-    /// @param  payees_   Address to receive all Orb proceeds.
-    /// @param  shares_   Orb name, used in ERC-721 metadata.
+    /// @dev    Calls the initializer of the `PaymentSplitterUpgradeable` contract, with payees and their shares.
+    /// @param  payees_   Payees addresses.
+    /// @param  shares_   Payees shares.
     function initialize(address[] memory payees_, uint256[] memory shares_) public initializer {
         __PaymentSplitter_init(payees_, shares_);
     }
