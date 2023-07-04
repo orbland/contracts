f039502efb561b02395a5b270726ddcf79f2fb18
Jonas Lekevicius <jonas@lekevicius.com>
2023-07-05 00:51:09 +0300

    Removing UUPS implementation note
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

--- src/CustomUUPSUpgradeable.sol
+++ src/CustomUUPSUpgradeable.sol
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.9.0) (proxy/utils/UUPSUpgradeable.sol)
-
 pragma solidity ^0.8.20;
 
 import {IERC1822ProxiableUpgradeable} from
