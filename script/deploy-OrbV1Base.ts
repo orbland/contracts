51af68d78cd20c6786ae54b8e008a70290869036
Jonas Lekevicius <jonas@lekevicius.com>
2023-07-08 19:27:41 +0300

    Deployment scripts
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

---
+++ script/deploy-OrbV1Base.ts
@@ -0,0 +1,22 @@
+import { TransactionResponse } from "ethers"
+import { ethers, upgrades } from "hardhat"
+
+async function main() {
+    const Orb = await ethers.getContractFactory("Orb")
+    const orbDeployTx = (await upgrades.deployImplementation(Orb, {
+        getTxResponse: true,
+        unsafeAllow: ["delegatecall"],
+    })) as TransactionResponse
+    const orbTxReceipt = await orbDeployTx.wait()
+    if (!orbTxReceipt) {
+        throw new Error("Cannot verify tx receipt")
+    }
+    const orbAddress = orbTxReceipt.contractAddress
+    console.log("Orb V1 implementation deployed to:", orbAddress)
+}
+
+// We recommend this pattern to be able to use async/await everywhere and properly handle errors.
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
