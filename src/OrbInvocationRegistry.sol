7955ccc3c983c925780d5ee46f888378f75efa47
Jonas Lekevicius <jonas@lekevicius.com>
2023-07-15 15:07:23 +0300

    Removing unused imports
    Signed-off-by: Jonas Lekevicius <jonas@lekevicius.com>

--- src/OrbInvocationRegistry.sol
+++ src/OrbInvocationRegistry.sol
@@ -5,7 +5,6 @@ import {IERC165Upgradeable} from
     "../lib/openzeppelin-contracts-upgradeable/contracts/utils/introspection/IERC165Upgradeable.sol";
 import {ERC165Upgradeable} from
     "../lib/openzeppelin-contracts-upgradeable/contracts/utils/introspection/ERC165Upgradeable.sol";
-import {Initializable} from "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 import {AddressUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/utils/AddressUpgradeable.sol";
